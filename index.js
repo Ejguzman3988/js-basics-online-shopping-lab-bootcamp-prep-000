27f43dcf882fac86a44b5f5627498655cfcfb2fa
Eriberto Guzman <eriberto176@gmail.com>
2020-04-07 05:30:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -69,8 +69,6 @@ function removeFromCart(item) {
   for(let i = 0; i < cart.length; i++){
     if (item === cart[i].itemName){
       cart.splice(i, 1);
-      newArray = cart;
-      
       return newArray;
     }
   }
