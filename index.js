0eb82ec291a080b5027e68787d9f01d6de166bed
Eriberto Guzman <eriberto176@gmail.com>
2020-04-07 05:15:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -87,7 +87,7 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   // write your code here
   
-  if (cardNumber === NaN){
+  if (cardNumber === 0){
     return "Sorry, we don't have a credit card on file for you."
   }
   else{
