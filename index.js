eb1eca2ea5b83fa3442fd0dacd344026bd697816
Eriberto Guzman <eriberto176@gmail.com>
2020-04-07 05:12:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -91,8 +91,8 @@ function placeOrder(cardNumber) {
     return "Sorry, we don't have a credit card on file for you."
   }
   else{
-    var total = total();
+    var totalNum = total();
     setCart([]);
-    return `Your total cost is $${total}, which will be charged to the card ${cardNumber}.` 
+    return `Your total cost is $${totalNum}, which will be charged to the card ${cardNumber}.` 
   }
 }
